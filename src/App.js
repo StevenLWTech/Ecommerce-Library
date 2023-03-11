242ec98fc8ec86dd56b10740aa3e019e6854d418
Steven <s-steven.truong@lwtech.edu>
2023-03-11 03:15:11 -0800

    init commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,49 @@ import Home from './pages/Home';
 import Books from './pages/Books';
 import {books} from "./data";
 import BookInfo from './pages/BookInfo';
+import Cart from './pages/Cart';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
+  const [cart, setCart] = useState([]);
+ 
+  function addToCart(book) {
+    setCart([...cart, {...book, quantity: 1}])
+  }
+  function changeQuantity(book, quantity){
+    setCart(cart.map(item => {
+      if(item.id === book.id) {
+        return {
+          ...item,
+          quantity: +quantity,
+        }
+      }
+      else {
+        return item;
+      }
+    }))
+  }
+  function removeBook(book){
+    console.log('rmeove item', book);
+    setCart(cart.filter(item => item.id !== book.id))
+  }
+  function numberOfItems(){
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity;
+    })
+    return counter;
+  }
+
   return (
     <Router>
     <div className="App">
-      <Nav/>
+      <Nav numberOfItems={numberOfItems()}/>
       <Route path ="/" exact component={Home}/>
       <Route path ="/books" exact render={()=><Books books={books}/>}/>
-      <Route path="/books/1" render={() => <BookInfo books={books}/>} />
+      <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart}/>}/>
+      <Route path ="/cart" render={()=><Cart books={books} cart ={cart} changeQuantity={changeQuantity} removeBook = {removeBook}/>}/>
       <Footer/>
     </div>
     </Router>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from "../assets/Library.svg";
 import { Link } from "react-router-dom";
-function Nav() {
-  function openMenu(){
+function Nav({ numberOfItems }) {
+  function openMenu() {
     document.body.classList += " menu--open";
   }
-  function closeMenu(){
+  function closeMenu() {
     document.body.classList.remove("menu--open");
   }
   return (
@@ -29,33 +29,35 @@ function Nav() {
           <button className="btn__menu" onClick={openMenu}>
             <FontAwesomeIcon icon="bars" />
           </button>
-          <li className="nav__icon"></li>
-          <Link to="/cart" className="nav__link">
-            <FontAwesomeIcon icon="shopping-cart"></FontAwesomeIcon>
-          </Link>
-          <span className="cart__lenth">2</span>
+          <li className="nav__icon">
+            <Link to="/cart" className="nav__link">
+              <FontAwesomeIcon icon="shopping-cart"></FontAwesomeIcon>
+            </Link>
+
+            {numberOfItems > 0 && <span className="cart__length">{numberOfItems}</span>}
+          </li>
         </ul>
         <div className="menu__backdrop">
-            <button className="btn__menu btn__menu--close" onClick={closeMenu}>
-                <FontAwesomeIcon icon="times"></FontAwesomeIcon>
-            </button>
-            <ul className="menu__links">
-                <li className="menu__list">
-                    <Link to="/" className="menu__link">
-                        Home
-                    </Link>
-                </li>
-                <li className="menu__list">
-                    <Link to="/books" className="menu__link">
-                        Books
-                    </Link>
-                </li>
-                <li className="menu__list">
-                    <Link to="/cart" className="menu__link">
-                        Cart
-                    </Link>
-                </li>
-            </ul>
+          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
+            <FontAwesomeIcon icon="times"></FontAwesomeIcon>
+          </button>
+          <ul className="menu__links">
+            <li className="menu__list">
+              <Link to="/" className="menu__link" onClick={closeMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="menu__list">
+              <Link to="/books" className="menu__link" onClick={closeMenu}>
+                Books
+              </Link>
+            </li>
+            <li className="menu__list">
+              <Link to="/cart" className="menu__link" onClick={closeMenu}>
+                Cart
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
